2793d72ab6cc6557ac9456271581b8686cbd50b3
arghya-bandyopadhyay-30 <arghya.bandyopadhyay.official1@gmail.com>
2024-11-24 12:41:47 +0530

    feat: add automated commits with random dates and improved error handling
    - Added functionality to create commits with random dates.
    - Implemented pull and rebase before each push to avoid conflicts.
    - Enhanced error handling with retries for push operations.

---
+++ src/main.py
@@ -0,0 +1,30 @@
+from src.utils import START_DATE, END_DATE, NUM_COMMITS, FILE_PATH, repo, COMMIT_MESSAGE, write_json, generate_random_dates
+
+def commit_and_push_for_date(date):
+    formatted_date = date.isoformat()
+    write_json(formatted_date)
+
+    repo.index.add([str(FILE_PATH)])
+    repo.index.commit(COMMIT_MESSAGE.format(formatted_date=formatted_date), author_date=formatted_date, commit_date=formatted_date)
+    print(f"Changes committed with custom date: {formatted_date}")
+
+    origin = repo.remote(name='origin')
+    try:
+        origin.pull('main', rebase=True)
+        origin.push(refspec='main')
+        print("Changes pushed to the remote repository.")
+    except Exception as e:
+        print(f"Error pushing to the remote repository: {e}")
+
+def main():
+    commit_dates = generate_random_dates(START_DATE, END_DATE, NUM_COMMITS)
+
+    for date in commit_dates:
+        try:
+            commit_and_push_for_date(date)
+        except Exception as e:
+            print(f"Error committing for date {date}: {e}")
+
+if __name__ == "__main__":
+    main()
+    print("All randomly selected dates have been processed and pushed.")
---
+++ src/utils.py
@@ -0,0 +1,33 @@
+import json
+from datetime import datetime, timedelta
+from pathlib import Path
+import random
+from git import Repo
+
+REPO_PATH = Path(__file__).resolve().parent.parent
+FILE_PATH = REPO_PATH / 'data.json'
+START_DATE = datetime(2024, 9, 4)
+END_DATE = datetime.now()
+NUM_COMMITS = random.randint(5, (END_DATE - START_DATE).days)
+repo = Repo(REPO_PATH)
+COMMIT_MESSAGE = """Updated data.json with timestamp: {formatted_date}"""
+
+def generate_random_dates(start, end, num_dates):
+    random_dates = set()
+    days_difference = (end - start).days
+
+    while len(random_dates) < num_dates:
+        random_day_offset = random.randint(0, days_difference)
+        random_date = start + timedelta(days=random_day_offset)
+        random_dates.add(random_date)
+
+    return list(random_dates)
+
+def write_json(date):
+    data_to_write = {
+        "date": date
+    }
+
+    with open(FILE_PATH, 'w') as json_file:
+        json.dump(data_to_write, json_file, indent=2)
+        print(f"Data successfully written for date: {date}")
